b9a0c7c127c2f0786a6f27b050358061ab73fd17
r34d02ly <r34d02ly@gmail.com>
2023-06-15 18:39:20 +0900

    The LeetCode Beginner's Guide 진행중 23.06.15

--- Study/src/Easy/RansomNote.java
+++ Study/src/Easy/RansomNote.java
@@ -1,13 +1,46 @@
 package Easy;
 
+import java.util.HashMap;
+
 public class RansomNote {
 
     String ransomNote = "baa", magazine = "aab";
 
     public boolean canConstruct01(String ransomNote, String magazine) {
         for(char c : ransomNote.toCharArray()){
+            if(magazine.equals("")) return false;
+            if(!magazine.contains(String.valueOf(c))) return false;
 
+            magazine = magazine.replaceFirst(String.valueOf(c), "");
         }
+        return true;
+
+    }
+
+    public boolean canConstruct02(String ransomNote, String magazine) {
+
+        if(ransomNote.length() > magazine.length()) return false;
+
+        // map에다가 magazine의 각 알바벳의 카운트를 세어서 저장함
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for(char c : magazine.toCharArray()) {
+            if(map.get(c) == null) {
+                map.put(c, 1);
+            } else {
+                map.put(c, map.get(c) + 1);
+            }
+        }
+
+        for(char c : ransomNote.toCharArray()){
+            if(map.get(c) == null) return false;
+
+            if(map.get(c) == 0) return false;
+
+            map.put(c, map.get(c) - 1);
+        }
+
+        return true;
+
     }
 
     public void methodTest() {
